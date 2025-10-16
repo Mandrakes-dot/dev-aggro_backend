ba344a5c28413c14f08051c16eecb1b28e135881
tom <tomtom.kazc@hotmail.com>
2025-10-16 23:28:41 +0200

    test

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import * as process from 'node:process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
-  console.log(process.env.PORT);
+  const port = Number(process.env.PORT) || 3000;
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
