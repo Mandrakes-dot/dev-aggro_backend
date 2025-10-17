a0fba3ba9ad974e5f6dbaf23bb51938012e37d77
tom <tomtom.kazc@hotmail.com>
2025-10-17 14:41:38 +0200

    feat: implement swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as process from 'node:process';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { apiReference } from '@scalar/nestjs-api-reference';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = Number(process.env.PORT) || 3000;
   await app.listen(port, '0.0.0.0');
+
+  const config = new DocumentBuilder()
+    .setTitle('DEV_AGRO API')
+    .setDescription('Routes description of Dev-agro API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api/doc-swagger', app, document, {
+    swaggerOptions: { persistAuthorization: true, filter: true },
+  });
+  app.use(
+    '/api/doc',
+    apiReference({ spec: { content: document }, theme: 'moon' }),
+  );
 }
-bootstrap();
+void bootstrap();
